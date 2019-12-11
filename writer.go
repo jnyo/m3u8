edda6d50b46ccbfd0e1baa9f7fd6b61df4a28704
jnyo <jnyo@users.noreply.github.com>
2019-12-11 14:50:45 +0900

    fix: change bitSize 32 to 64 for EXTINF

--- writer.go
+++ writer.go
@@ -689,7 +689,7 @@ func (p *MediaPlaylist) Encode() *bytes.Buffer {
 				durationCache[seg.Duration] = strconv.FormatInt(int64(math.Ceil(seg.Duration)), 10)
 			} else {
 				// Wowza Mediaserver and some others prefer floats.
-				durationCache[seg.Duration] = strconv.FormatFloat(seg.Duration, 'f', -1, 32)
+				durationCache[seg.Duration] = strconv.FormatFloat(seg.Duration, 'f', -1, 64)
 			}
 			p.buf.WriteString(durationCache[seg.Duration])
 		}
